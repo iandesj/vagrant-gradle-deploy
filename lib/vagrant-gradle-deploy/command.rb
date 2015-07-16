8293f0824f942ec87e27e9866ed9736d0bf32eca
Ian DesJardins <ian.d.desjardins@gmail.com>
2015-07-16 10:19:13 -0400

    0.0.7

--- lib/vagrant-gradle-deploy/command.rb
+++ lib/vagrant-gradle-deploy/command.rb
@@ -1,8 +1,12 @@
 module Vagrant
     module GradleDeploy
         class Command < Vagrant.plugin('2', :command)
-            def execute
-                exec("vagrant ssh -c 'sudo su - root -c \"cd /var/lib/tomcat8/webapps && gradle clean build war; gradle deploy\"'")
+            def execute(*args)
+                if args.include?("--no-test")
+                    exec("vagrant ssh -c 'sudo su - root -c \"cd /var/lib/tomcat8/webapps && gradle clean build war -x test; gradle deploy\"'")
+                else
+                    exec("vagrant ssh -c 'sudo su - root -c \"cd /var/lib/tomcat8/webapps && gradle clean build war; gradle deploy\"'")
+                end
                 0
             end
         end
--- lib/vagrant-gradle-deploy/version.rb
+++ lib/vagrant-gradle-deploy/version.rb
@@ -1,5 +1,5 @@
 module Vagrant
     module GradleDeploy
-        VERSION='0.0.6'
+        VERSION='0.0.7'
     end
 end
